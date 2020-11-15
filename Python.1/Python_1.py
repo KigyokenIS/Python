83d140a533044ef379fcc11593875740153ec3c2
KIS <74498860+KigyokenIS@users.noreply.github.com>
2020-11-16 01:32:09 +0900

    Update Python_1.py

--- Python.1/Python_1.py
+++ Python.1/Python_1.py
@@ -1,1 +1,16 @@
+print("Hello World!")
 
+hw_str = "Hello World!"
+print(hw_str)
+
+result_int = 1+2
+print("1+2={}".format(result_int))
+
+result_int = 1+2
+print("1+2=",result_int)
+print("1+2="+str(result_int))
+print("1+2=%d"%(result_int))
+
+print("1+2={}".format(result_int))
+
+print("1+2=",result_int)
